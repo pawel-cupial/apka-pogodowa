18217f81f491ed852fbd95d23fa8e1d55265a194
pawel-cupial <p.cupial@outlook.com>
2019-11-03 18:06:07 +0100

    rewrite script

--- script.js
+++ script.js
@@ -17,111 +17,125 @@ const searchButton = document.getElementById('search-button')
 const arrowIcon = document.getElementById('arrow')
 const country = document.getElementById('location-info-container__country')
 const coords = document.getElementById('location-info-container__coords')
+const metricBtn = document.getElementById('temp-choice1')
+const imperialBtn = document.getElementById('temp-choice2')
 
 const nameSpace = {
-weatherUrl: `https://api.openweathermap.org/data/2.5/weather?q=zabrze&units=metric&appid=${weatherKey}&lang=pl`,
-inputText: '',
-searchText: '',
-windDirection: ''
+    inputText: '',
+    searchText: 'zabrze',
+    windDirection: '',
+    units: 'metric',
+    tempUnits: '\u2103',
+    speedUnits: ' m/s'
+    }
+
+
+const getCity = () => {
+    searchField.addEventListener('input', (e) => {
+        nameSpace.inputText = e.target.value
+    })
+    
+    searchButton.addEventListener('click', () => {
+        nameSpace.searchText = nameSpace.inputText
+        getWeather(nameSpace.searchText, nameSpace.units)
+        searchField.value = ''
+    })
 }
 
-const methods = {
-    getCity() {
-        searchField.addEventListener('input', (e) => {
-            nameSpace.inputText = e.target.value
-        })
-        
-        searchButton.addEventListener('click', () => {
-            nameSpace.searchText = nameSpace.inputText
-            nameSpace.weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${nameSpace.searchText}&units=metric&appid=${weatherKey}&lang=pl`
-            this.getWeather()
-            searchField.value = ''
-        })
-    },
-
-    getWeather() {
-        this.getCity()
-        fetch(`${nameSpace.weatherUrl}`).then(resp => {
-            return resp.json().then(resp => {
-                this.renderFetchedData(resp)
-                this.changeBackground(resp)
-            }).catch(error => {
-                alert('Niestety, Twojej miejscowości nie ma na liśćie :( Wprowadź nazwę innej miejscowości')
-            })
-        })
-    },
-
-    renderFetchedData(data) {
-        city.textContent = data.name;
-        icon.setAttribute('src', `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
-        description.textContent = data.weather[0].description.charAt(0).toUpperCase() + data.weather[0].description.slice(1);
-        temp.textContent = Math.round(data.main.temp);
-        tempMax.innerHTML =  Math.round(data.main.temp_max) + "\u2103";
-        tempMin.innerHTML = Math.round(data.main.temp_min) + "\u2103";
-        humidity.textContent = data.main.humidity + ' %';
-        pressure.textContent = data.main.pressure + ' hPa'; 
-        windSpeed.textContent = data.wind.speed + ' km/h';
-        currentDate.textContent = moment().format('dddd, D MMMM YYYY, h:mm');
-        dayOfTheYear.textContent = moment().format('DDD') + ' dzień roku'
-        let sunsetHours = new Date(data.sys.sunset*1000).getHours();
-        sunsetHours = ("0" + sunsetHours).slice(-2);
-        let sunsetMinutes = new Date(data.sys.sunset*1000).getMinutes();
-        sunsetMinutes = ("0" + sunsetMinutes).slice(-2);
-        sunset.textContent = sunsetHours + ':' + sunsetMinutes;
-        let sunriseHours = new Date(data.sys.sunrise*1000).getHours();
-        sunriseHours = ("0" + sunriseHours).slice(-2);
-        let sunriseMinutes = new Date(data.sys.sunrise*1000).getMinutes();
-        sunriseMinutes = ("0" + sunriseMinutes).slice(-2);
-        sunrise.textContent = sunriseHours + ':' + sunriseMinutes;
-        nameSpace.windDirection = data.wind.deg;
-        country.textContent = data.sys.country;
-        coords.innerHTML = data.coord.lat + '&deg;' + ', ' + data.coord.lon + '&deg;';
-    },
-
-    changeBackground(data) {
-        switch (data.weather[0].main) {
-            case 'Thunderstorm':
-                document.body.style.backgroundImage = "url('img/thunderstorm.jpg)";
-                break;
-            case 'Clouds':
-                document.body.style.backgroundImage = "url(img/clouds.jpg)";
-                break;
-            case 'Clear':
-                document.body.style.backgroundImage = "url(img/sun2.jpg)";
-                break;
-            case 'Snow':
-                document.body.style.backgroundImage = "url(img/snow.jpg)";
-                break;
-            case 'Rain':
-                document.body.style.backgroundImage = "url(img/rain.jpg)";
-                break;
-            case 'Drizzle':
-                document.body.style.backgroundImage = "url(img/rain.jpg)";
-                break;
-            case 'Mist':
-                document.body.style.backgroundImage = "url(img/fog.jpg)";
-                break;
-            case 'Fog':
-                document.body.style.backgroundImage = "url(img/fog.jpg)";
-                break;
-            case 'Haze':
-                document.body.style.backgroundImage = "url(img/fog.jpg)";
-                break;
-        }
-    },
+const renderFetchedData = (data) => {
+    city.textContent = data.name;
+    icon.setAttribute('src', `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+    description.textContent = data.weather[0].description.charAt(0).toUpperCase() + data.weather[0].description.slice(1);
+    temp.textContent = Math.round(data.main.temp);
+    tempMax.innerHTML =  Math.round(data.main.temp_max) + `${nameSpace.tempUnits}`;
+    tempMin.innerHTML = Math.round(data.main.temp_min) + `${nameSpace.tempUnits}`;
+    humidity.textContent = data.main.humidity + ' %';
+    pressure.textContent = data.main.pressure + ' hPa'; 
+    windSpeed.textContent = data.wind.speed + `${nameSpace.speedUnits}`;
+    currentDate.textContent = moment().format('dddd, D MMMM YYYY, h:mm');
+    dayOfTheYear.textContent = moment().format('DDD') + ' dzień roku'
+    let sunsetHours = new Date(data.sys.sunset*1000).getHours();
+    sunsetHours = ("0" + sunsetHours).slice(-2);
+    let sunsetMinutes = new Date(data.sys.sunset*1000).getMinutes();
+    sunsetMinutes = ("0" + sunsetMinutes).slice(-2);
+    sunset.textContent = sunsetHours + ':' + sunsetMinutes;
+    let sunriseHours = new Date(data.sys.sunrise*1000).getHours();
+    sunriseHours = ("0" + sunriseHours).slice(-2);
+    let sunriseMinutes = new Date(data.sys.sunrise*1000).getMinutes();
+    sunriseMinutes = ("0" + sunriseMinutes).slice(-2);
+    sunrise.textContent = sunriseHours + ':' + sunriseMinutes;
+    nameSpace.windDirection = data.wind.deg;
+    country.textContent = data.sys.country;
+    coords.innerHTML = data.coord.lat + '&deg;' + ', ' + data.coord.lon + '&deg;';
 }
+
+const changeBackground = (data) => {
+    switch (data.weather[0].main) {
+        case 'Thunderstorm':
+            document.body.style.backgroundImage = "url('img/thunderstorm.jpg)";
+            break;
+        case 'Clouds':
+            document.body.style.backgroundImage = "url(img/clouds.jpg)";
+            break;
+        case 'Clear':
+            document.body.style.backgroundImage = "url(img/sun2.jpg)";
+            break;
+        case 'Snow':
+            document.body.style.backgroundImage = "url(img/snow.jpg)";
+            break;
+        case 'Rain':
+            document.body.style.backgroundImage = "url(img/rain.jpg)";
+            break;
+        case 'Drizzle':
+            document.body.style.backgroundImage = "url(img/rain.jpg)";
+            break;
+        case 'Mist':
+            document.body.style.backgroundImage = "url(img/fog.jpg)";
+            break;
+        case 'Fog':
+            document.body.style.backgroundImage = "url(img/fog.jpg)";
+            break;
+        case 'Haze':
+            document.body.style.backgroundImage = "url(img/fog.jpg)";
+            break;
+    }
+}
+
+const getWeather = (city='zabrze', units='metric') => {
+    getCity()
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${weatherKey}&lang=pl`).then(resp => {
+        return resp.json().then(resp => {
+            renderFetchedData(resp)
+            changeBackground(resp)
+        }).catch(error => {
+            throw(alert(error))
+        })
+    })
+}
+
 
 searchField.addEventListener('keyup', (e) => {
     if (e.keyCode === 13) {
         searchButton.click()
+        getWeather(nameSpace.searchText, nameSpace.units)
     }
 })
 
-window.onload = (e) => {
-    setInterval(() => {
-      arrowIcon.style.transform = `rotate(${nameSpace.windDirection}deg)`;
-    }, 500)
-  }
+imperialBtn.addEventListener('change', (e) =>{
+    if(e.target.checked) {
+        nameSpace.units = 'imperial'
+        nameSpace.tempUnits = '\u2109'
+        nameSpace.speedUnits = ' mph'
+        getWeather(nameSpace.searchText, nameSpace.units)
+    }
+})
 
-moment.locale("pl"); 
-methods.getWeather()
+metricBtn.addEventListener('change', (e) =>{
+    if(e.target.checked) {
+        nameSpace.units = 'metric'
+        nameSpace.tempUnits = '\u2103'
+        nameSpace.speedUnits = ' m/s'
+        getWeather(nameSpace.searchText, nameSpace.units)
+    }
+})
+getWeather()
